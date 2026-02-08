8b19d8f13663fc9cd658f5a4ce7f340b7030a53c
Eduardo Neira <eduardo.neira31@gmail.com>
2026-02-08 10:55:50 -0300

    puerto configurable por variable de entorno

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const app = require('./src/app'); // Importa la configuración de Express desde app.js
 
 // Puerto configurable por variable de entorno o por defecto 3000
-const PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3000;
+PORT = parseInt(PORT, 10);
+if (isNaN(PORT) || PORT < 0 || PORT > 65535) {
+  console.error('PORT variable must be an integer between 0 and 65535');
+  process.exit(1);
+}
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT} 🚀`);
